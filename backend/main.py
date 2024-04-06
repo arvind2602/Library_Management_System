954a55796636dfda705df27b7dfcf768164fc575
arvind2602 <guptaarvind2602@gmail.com>
2024-04-06 15:28:43 +0530

    Added Databse to it

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pymongo import MongoClient;
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client=MongoClient(os.getenv("MONGO_DB"))
+print(client.list_database_names())
 
 app = FastAPI()
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Wrld"}
